88dbd893572d459a5b24f2d3227f64e2ca744bc1
world.cyp <world.sk137@gmail.com>
2023-01-21 18:57:00 +0700

    css18

--- api/index.php
+++ api/index.php
@@ -1,21 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-</head>
-<link rel="stylesheet" href="../public/index.css">
-
-<body>
-    <?php
-    require __DIR__ . '/../public/index.php';
-    ?>
-    <h1 class="text">hello world</h1>
-</body>
-<script type="text/javascript" src="../public/index.js"></script>
-
-</html>
+<?php
+require __DIR__ . '/../public/index.php';
