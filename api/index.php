343176333703f37d3184688eb10f01bb1adfd153
world.cyp <world.sk137@gmail.com>
2023-01-21 18:23:52 +0700

    css10

--- api/index.php
+++ api/index.php
@@ -6,12 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./public/index.css">
+    <link rel="stylesheet" href="../public/index.css">
 </head>
 
 <body>
     
     <h1 class="text">hello world</h1>
 </body>
-<script type="text/javascript" src="./public/index.js"></script>
+<script type="text/javascript" src="../public/index.js"></script>
 </html>
