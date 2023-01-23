6a93c0d76c0deacf1ac4da8617978407d1c70576
world.cyp <world.sk137@gmail.com>
2023-01-23 10:35:30 +0700

    css25

--- public/index.php
+++ public/index.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
+    <link rel="stylesheet" href="../public/index.css">
 </head>
-<link rel="stylesheet" href="<?php echo __DIR__; ?>../public/index.css">
+
 
 <body>
     <h1 class="text">hello world</h1>
